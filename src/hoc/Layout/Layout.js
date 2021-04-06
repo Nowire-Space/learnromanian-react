e7975247ac63367d3a0bcebbfb72213ae8233ee5
NikiforiakMykola <57327282+NikiforiakMykola@users.noreply.github.com>
2021-04-06 16:34:38 +0300

    Default configurations
    Enable CSS modules
    Create project structure
    Add Aux component
    Add Layout component

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import Aux from '../Aux/Aux';
+import layoutClasses from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    render() {
+        return (null);
+    }
+}
+
+export default Layout;
